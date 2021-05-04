6b405fce15e02cd5b6f787b412884c9c616803e5
naresh2009 <74289116+naresh2009@users.noreply.github.com>
2021-05-04 14:43:02 +0530

    Add files via upload

---
+++ carRacingStage3-master/carRacingStage3-master/sketch.js
@@ -0,0 +1,55 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+var world,engine;
+
+
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var cars, car1, car2, car3, car4;
+
+var track, car1_img, car2_img, car3_img, car4_img;
+
+function preload(){
+  track = loadImage("images/track.jpg");
+  car1_img = loadImage("images/bike1.png");
+  car2_img = loadImage("images/bike2.png");
+  car3_img = loadImage("images/bike3.png");
+  car4_img = loadImage("images/bike4.png");
+  ground = loadImage("images/ground.png");
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  engine = Engine.create();
+  world = engine.world;
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  Engine.run(engine);
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+  if(gameState === 2){
+    game.end();
+  }
+}
